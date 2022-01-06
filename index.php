053b420e061168f426d3dc1c46b61d99cebd26b2
Schepa337 <katyaoschepkova@yandex.ru>
2022-01-06 13:28:05 +0500

    furnituregood
    furnituregood

---
+++ index.php
@@ -0,0 +1,382 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>МебельГуд</title>
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="/main.css">
+	<link href="/js/owl/assets/owl.carousel.min.css" rel="stylesheet">
+	<link href="/js/owl/assets/owl.theme.default.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="/js/flister/jquery.flipster.min.css">
+	<meta property="og:description" content="МебельГуд - гипермаркет мебели" />
+	<meta name="description" content="МебельГуд - гипермаркет мебели" />
+	<meta name="mrc__share_title" content="Forrest" />
+	<meta name="mrc__share_description" content="МебельГуд - гипермаркет мебели" />
+	<meta property="og:title" content="МебельГуд" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="" />
+	<meta property="og:site_name" content="МебельГуд" />
+	<meta property="og:locale" content="ru" />
+	<meta property="og:image" content="/images/logo.png" />
+	<meta property="og:image:alt" content="МебельГуд" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" type="image/ico" href="/favicon.ico"/>
+</head>
+<body>
+	<header>
+		<div class="container-header">
+			<div class="left">
+				<a href="/" class="logo">
+					<img class="logo" src="/images/logo.png" alt="good">
+				</a>
+			</div>
+			<div class="right">
+				<div class="contacts">
+					+7 (495) 995 94 96
+				</div>
+				<div class="hours">
+					<img class="passage" src="/images/passage.png" alt="good">
+					Режим работы с 10:00 до 20:00
+				</div>
+				<ul class="menu">
+					<li><a href="#centr">О ЦЕНТРЕ</a></li>
+					<li><a href="#shops">МАГАЗИНЫ</a></li>
+					<li><a href="#scheme">СХЕМА</a></li>
+					<li><a href="#stocks">АКЦИИ</a></li>
+					<li><a href="#contacts">КОНТАКТЫ</a></li>
+					<li class="searchToggleWrap"><img id="searchToggle" class="search" src="/images/search.png" alt="good"></li>
+					<li class="searchbox">
+						<input type="text" placeholder="Вы ищите...">
+					</li>
+				</ul>
+				<div class="social">
+					<a class="icon icon-vk" target="_blank" href="#">
+						<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+						viewBox="0 0 511.962 511.962" style="enable-background:new 0 0 511.962 511.962;" xml:space="preserve">
+						<g>
+							<g>
+								<path d="M507.399,370.471c-1.376-2.304-9.888-20.8-50.848-58.816c-42.88-39.808-37.12-33.344,14.528-102.176
+								c31.456-41.92,44.032-67.52,40.096-78.464c-3.744-10.432-26.88-7.68-26.88-7.68l-76.928,0.448c0,0-5.696-0.768-9.952,1.76
+								c-4.128,2.496-6.784,8.256-6.784,8.256s-12.192,32.448-28.448,60.032c-34.272,58.208-48,61.28-53.6,57.664
+								c-13.024-8.416-9.76-33.856-9.76-51.904c0-56.416,8.544-79.936-16.672-86.016c-8.384-2.016-14.528-3.36-35.936-3.584
+								c-27.456-0.288-50.72,0.096-63.872,6.528c-8.768,4.288-15.52,13.856-11.392,14.4c5.088,0.672,16.608,3.104,22.72,11.424
+								c7.904,10.72,7.616,34.848,7.616,34.848s4.544,66.4-10.592,74.656c-10.4,5.664-24.64-5.888-55.2-58.72
+								c-15.648-27.04-27.488-56.96-27.488-56.96s-2.272-5.568-6.336-8.544c-4.928-3.616-11.84-4.768-11.84-4.768l-73.152,0.448
+								c0,0-10.976,0.32-15.008,5.088c-3.584,4.256-0.288,13.024-0.288,13.024s57.28,133.984,122.112,201.536
+								c59.488,61.92,127.008,57.856,127.008,57.856h30.592c0,0,9.248-1.024,13.952-6.112c4.352-4.672,4.192-13.44,4.192-13.44
+								s-0.608-41.056,18.464-47.104c18.784-5.952,42.912,39.68,68.48,57.248c19.328,13.28,34.016,10.368,34.016,10.368l68.384-0.96
+								C488.583,400.807,524.359,398.599,507.399,370.471z"/>
+							</g>
+						</g>
+					</svg>
+				</a>
+				<a class="icon icon-instagram" target="_blank" href="#">
+					<svg viewBox="-21 -21 682.66669 682.66669" xmlns="http://www.w3.org/2000/svg">
+						<path d="m0 132.976562v374.046876c0 73.441406 59.535156 132.976562 132.976562 132.976562h374.046876c73.441406 0 132.976562-59.535156 132.976562-132.976562v-374.046876c0-73.441406-59.535156-132.976562-132.976562-132.976562h-374.046876c-73.441406 0-132.976562 59.535156-132.976562 132.976562zm387.792969 368.359376c-157.855469 54.464843-303.59375-91.273438-249.128907-249.128907 18.351563-53.203125 60.335938-95.191406 113.539063-113.542969 157.859375-54.464843 303.597656 91.273438 249.132813 249.132813-18.351563 53.203125-60.335938 95.1875-113.542969 113.539063zm154.28125-374.859376c-2.511719 13.152344-13.394531 20.804688-24.652344 20.804688-6.851563 0-13.835937-2.828125-19.183594-8.964844-.472656-.542968-.914062-1.125-1.304687-1.730468-5.519532-8.4375-5.691406-18.460938-1-26.589844 3.320312-5.753906 8.679687-9.863282 15.097656-11.582032 6.410156-1.726562 13.113281-.839843 18.859375 2.484376 8.132813 4.6875 12.992187 13.457031 12.4375 23.511718-.039063.6875-.121094 1.386719-.253906 2.066406zm0 0"/>
+						<path d="m320 164.523438c-85.734375 0-155.476562 69.742187-155.476562 155.476562s69.742187 155.476562 155.476562 155.476562 155.476562-69.742187 155.476562-155.476562-69.742187-155.476562-155.476562-155.476562zm0 0"/>
+					</svg>
+				</a>
+			</div>
+		</div>
+	</div>
+	<div class="container-header-mobile">
+		<div class="top">
+			<a href="/" class="logo">
+				<img class="logo" src="/images/logo.png" alt="good">
+			</a>
+			<div class="hours">
+				<img class="passage" src="/images/passage.png" alt="good">
+				Режим работы с 10:00 до 20:00
+			</div>						
+		</div>
+		<div class="bottom">
+			<div class="controls">
+				<div class="search-block">
+					<img class="search" src="/images/search.png" alt="good">
+					<input type="text" placeholder="Вы ищите...">
+				</div>
+				<div class="menu-switch-wrap">
+					<svg id="menu-switch" enable-background="new 0 0 512 512" height="512" viewBox="0 0 512 512" width="512" xmlns="http://www.w3.org/2000/svg"><g><path d="m174 240h-108c-36.393 0-66-29.607-66-66v-108c0-36.393 29.607-66 66-66h108c36.393 0 66 29.607 66 66v108c0 36.393-29.607 66-66 66zm-108-208c-18.748 0-34 15.252-34 34v108c0 18.748 15.252 34 34 34h108c18.748 0 34-15.252 34-34v-108c0-18.748-15.252-34-34-34z"/><path d="m446 240h-108c-36.393 0-66-29.607-66-66v-108c0-36.393 29.607-66 66-66h108c36.393 0 66 29.607 66 66v108c0 36.393-29.607 66-66 66zm-108-208c-18.748 0-34 15.252-34 34v108c0 18.748 15.252 34 34 34h108c18.748 0 34-15.252 34-34v-108c0-18.748-15.252-34-34-34z"/><path d="m392 512c-66.168 0-120-53.832-120-120s53.832-120 120-120 120 53.832 120 120-53.832 120-120 120zm0-208c-48.523 0-88 39.477-88 88s39.477 88 88 88 88-39.477 88-88-39.477-88-88-88z"/><path d="m174 512h-108c-36.393 0-66-29.607-66-66v-108c0-36.393 29.607-66 66-66h108c36.393 0 66 29.607 66 66v108c0 36.393-29.607 66-66 66zm-108-208c-18.748 0-34 15.252-34 34v108c0 18.748 15.252 34 34 34h108c18.748 0 34-15.252 34-34v-108c0-18.748-15.252-34-34-34z"/></g></svg>
+				</div>
+			</div>
+
+			<ul class="menu menu-mobile">
+				<li><a href="#centr">О ЦЕНТРЕ</a></li>
+				<li><a href="#shops">МАГАЗИНЫ</a></li>
+				<li><a href="#scheme">СХЕМА</a></li>
+				<li><a href="#stocks">АКЦИИ</a></li>
+				<li><a href="#contacts">КОНТАКТЫ</a></li>
+			</ul>
+			<div class="social">
+				<a class="icon icon-vk" target="_blank" href="#">
+					<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+					viewBox="0 0 511.962 511.962" style="enable-background:new 0 0 511.962 511.962;" xml:space="preserve">
+					<g>
+						<g>
+							<path d="M507.399,370.471c-1.376-2.304-9.888-20.8-50.848-58.816c-42.88-39.808-37.12-33.344,14.528-102.176
+							c31.456-41.92,44.032-67.52,40.096-78.464c-3.744-10.432-26.88-7.68-26.88-7.68l-76.928,0.448c0,0-5.696-0.768-9.952,1.76
+							c-4.128,2.496-6.784,8.256-6.784,8.256s-12.192,32.448-28.448,60.032c-34.272,58.208-48,61.28-53.6,57.664
+							c-13.024-8.416-9.76-33.856-9.76-51.904c0-56.416,8.544-79.936-16.672-86.016c-8.384-2.016-14.528-3.36-35.936-3.584
+							c-27.456-0.288-50.72,0.096-63.872,6.528c-8.768,4.288-15.52,13.856-11.392,14.4c5.088,0.672,16.608,3.104,22.72,11.424
+							c7.904,10.72,7.616,34.848,7.616,34.848s4.544,66.4-10.592,74.656c-10.4,5.664-24.64-5.888-55.2-58.72
+							c-15.648-27.04-27.488-56.96-27.488-56.96s-2.272-5.568-6.336-8.544c-4.928-3.616-11.84-4.768-11.84-4.768l-73.152,0.448
+							c0,0-10.976,0.32-15.008,5.088c-3.584,4.256-0.288,13.024-0.288,13.024s57.28,133.984,122.112,201.536
+							c59.488,61.92,127.008,57.856,127.008,57.856h30.592c0,0,9.248-1.024,13.952-6.112c4.352-4.672,4.192-13.44,4.192-13.44
+							s-0.608-41.056,18.464-47.104c18.784-5.952,42.912,39.68,68.48,57.248c19.328,13.28,34.016,10.368,34.016,10.368l68.384-0.96
+							C488.583,400.807,524.359,398.599,507.399,370.471z"/>
+						</g>
+					</g>
+				</svg>
+			</a>
+			<a class="icon icon-instagram" target="_blank" href="#">
+				<svg viewBox="-21 -21 682.66669 682.66669" xmlns="http://www.w3.org/2000/svg">
+					<path d="m0 132.976562v374.046876c0 73.441406 59.535156 132.976562 132.976562 132.976562h374.046876c73.441406 0 132.976562-59.535156 132.976562-132.976562v-374.046876c0-73.441406-59.535156-132.976562-132.976562-132.976562h-374.046876c-73.441406 0-132.976562 59.535156-132.976562 132.976562zm387.792969 368.359376c-157.855469 54.464843-303.59375-91.273438-249.128907-249.128907 18.351563-53.203125 60.335938-95.191406 113.539063-113.542969 157.859375-54.464843 303.597656 91.273438 249.132813 249.132813-18.351563 53.203125-60.335938 95.1875-113.542969 113.539063zm154.28125-374.859376c-2.511719 13.152344-13.394531 20.804688-24.652344 20.804688-6.851563 0-13.835937-2.828125-19.183594-8.964844-.472656-.542968-.914062-1.125-1.304687-1.730468-5.519532-8.4375-5.691406-18.460938-1-26.589844 3.320312-5.753906 8.679687-9.863282 15.097656-11.582032 6.410156-1.726562 13.113281-.839843 18.859375 2.484376 8.132813 4.6875 12.992187 13.457031 12.4375 23.511718-.039063.6875-.121094 1.386719-.253906 2.066406zm0 0"/>
+					<path d="m320 164.523438c-85.734375 0-155.476562 69.742187-155.476562 155.476562s69.742187 155.476562 155.476562 155.476562 155.476562-69.742187 155.476562-155.476562-69.742187-155.476562-155.476562-155.476562zm0 0"/>
+				</svg>
+			</a>				
+		</div>
+	</div>
+</div>
+</div>
+</header>
+<section id="hypermarket" class="hypermarket">
+	<div class="icon">
+		<div class="ikonka">
+			<img class="ico" src="/images/parking-sign.png" alt="good">
+		</div>
+		<div class="ikonka">
+			<img class="ico" src="/images/distance.png" alt="good">
+		</div>
+		<div class="ikonka">
+			<img class="ico" src="/images/camera.png" alt="good">
+		</div>
+		<div class="ikonka">
+			<img class="ico" src="/images/armchair.png" alt="good">
+		</div>
+		<div class="ikonka">
+			<img class="ico" src="/images/credit-card.png" alt="good">
+		</div>
+		<div class="ikonka">
+			<img class="ico" src="/images/deal.png" alt="good">
+		</div>
+		<div class="ikonka">
+			<img class="ico" src="/images/sale.png" alt="good">
+		</div>
+	</div>
+</section>
+<section id="catalog" class="catalog">
+	<div class="left">
+		<div class="shop-block">
+			<div class="h1 title">Каталог мебели</div>
+			<div class="shop">
+				<a href="#">Магазины</a>
+			</div>			
+		</div>
+	</div>
+	<div class="right">
+		<div class="recliner">
+			<img class="img" src="/images/recliner.png" alt="good">
+		</div>
+		<div class="ellipse">
+			<img class="img" src="/images/ellipse.png" alt="good">
+		</div>
+	</div>
+</section>
+<section id="figures" class="figures">
+	<div class="top">
+		<div class="picture">
+			<div class="item">
+				<img class="ico" src="/images/locker.png" alt="good">
+				<div class= "text">Корпусная мебель</div>				
+			</div>
+			<div class="item">
+				<img class="ico" src="/images/interior-design.png" alt="good">
+				<div class= "text">Столы и стулья</div>			
+			</div>
+			<div class="item">
+				<img class="ico" src="/images/lounge.png" alt="good">
+				<div class= "text">Мягкая мебель</div>				
+			</div>
+			<div class="item">
+				<img class="ico" src="/images/bed.png" alt="good">
+				<div class= "text">Матрасы и кровати</div>				
+			</div>
+			<div class="item">
+				<img class="ico" src="/images/chandelier.png" alt="good">
+				<div class= "text">Люстры</div>				
+			</div>
+			<div class="item">
+				<img class="ico" src="/images/kitchen.png" alt="good">
+				<div class= "text">Кухни</div>				
+			</div>
+		</div>
+		<div class="picture_2">
+			<img class="ico" src="/images/squares.png" alt="good">
+		</div>
+	</div>
+	<div class="bottom">
+	</div>
+</section>
+<section id="slider" class="slider">
+	<div class="top">
+		<div class="left">
+			<div class="news">
+				<div class="h1 title">Новости</div>
+				<ul class="news-list">
+					<li>
+						<div class="date">1 мая</div>
+						<div class="desc">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.
+						</div>
+						<a href="#" class="more">Читать далее</a>
+					</li>
+					<li>
+						<div class="date">1 мая</div>
+						<div class="desc">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.
+						</div>
+						<a href="#" class="more">Читать далее</a>
+					</li>
+					<li>
+						<div class="date">1 мая</div>
+						<div class="desc">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.
+						</div>
+						<a href="#" class="more">Читать далее</a>
+					</li>								
+				</ul>
+			</div>
+		</div>
+		<div class="right">
+			<div class="h1 title">Акции</div>
+			<div class="pointer pointer_2">
+				<img class="pointer" src="/images/pointer.png" alt="good">
+			</div>
+			<div class="slider-aktsii owl-carousel owl-theme" id="slider">
+				<div><img src="images/slide.png" alt="просто картинка"></div>
+				<div><img src="images/slide.png" alt="просто картинка"></div>
+				<div><img src="images/slide.png" alt="просто картинка"></div>
+				<div><img src="images/slide.png" alt="просто картинка"></div>
+				<div><img src="images/slide.png" alt="просто картинка"></div>
+				<div><img src="images/slide.png" alt="просто картинка"></div>
+			</div>
+		</div>
+	</div>
+	<div class="bottom">
+		<div class="h1 title">Наши партнеры</div>
+
+		<div class="slider-partneri" id="slider-partneri">
+			<ul>
+				<li><img src="images/art.png" alt="просто картинка"></li>
+				<li><img src="images/Marya.png" alt="просто картинка"></li>
+				<li><img src="images/hoff.png" alt="просто картинка"></li>
+				<li><img src="images/mebel.png" alt="просто картинка"></li>
+				<li><img src="images/art.png" alt="просто картинка"></li>
+			</ul>
+		</div>
+		<div class="img-footer">
+			<img class="img-arrow" src="/images/footer-arrow.png" alt="arrow">
+			<img class="img" src="/images/footer.png" alt="good">
+		</div>
+	</div>
+</section>
+<footer>
+	<div class="footer-wrap">
+		<div class="left">
+			<div class="foot">
+				<div class="furnituregood">
+					<div class="logo-2">
+						<img class="img-logo" src="/images/logo_2.png" alt="logo">
+					</div>
+					<div class="rent">
+						<a href="#">Арендаторам</a>
+					</div>
+				</div>
+				<div class="information">
+
+				</div>
+			</div>
+		</div>
+		<div class="rest">
+			<div class="mid-left">
+				<div class="text-title">Информация</div>
+				<div class="text"><a href="#">О центре</a></div>
+				<div class="text"><a href="#">Магазины</a></div>
+				<div class="text"><a href="#">Схема ТЦ</a></div>
+				<div class="text"><a href="#">Акции</a></div>
+				<div class="text"><a href="#">Контакты</a></div>
+			</div>
+			<div class="mid-right">
+				<div class="text-title">Сервис</div>
+				<div class="text"><a href="#">Аренда</a></div>
+				<div class="text"><a href="#">Другие ТЦ</a></div>
+				<div class="text"><a href="#">События</a></div>
+				<div class="text"><a href="#">Медия</a></div>
+				<div class="text"><a href="#">Карта сайта</a></div>
+			</div>
+			<div class="right">
+				<div class="text-title">Контакты</div>
+				<div class="map">
+					Горьковского ш., 52 км,<br> 
+					Ногинск, Московская обл
+				</div>
+				<a class="call" href="tel:+74959959496">+7 (495) 995-94-96</a>	
+				<div class="social">
+					<a class="instagram" href="#"></a>
+					<a class="vk" href="#"></a>
+				</div>	
+			</div>
+			
+		</div>
+
+	</div>
+</footer>
+<script
+src="https://code.jquery.com/jquery-3.4.1.min.js"
+integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+crossorigin="anonymous"></script>
+<script src="/js/owl/owl.carousel.min.js"></script>
+<script src="/js/flister/jquery.flipster.min.js"></script>
+<script>
+	(function($){
+		$(function() {
+			$(".owl-carousel").owlCarousel({
+				items: 1,
+				nav: true,
+				loop: true
+			});
+		});
+	})(jQuery);
+
+	function initFlipper() {
+		if ($( window ).width() > 595) {
+			$('.slider-partneri').flipster({
+				style: 'flat',
+				start: 'center',
+				spacing: -.35,
+				scrollwheel: false,
+			})	
+		}	
+	}
+
+	initFlipper();
+
+	$( window ).resize(function() {
+		initFlipper();
+	});
+
+	$('#menu-switch').click(function() {
+		$('.menu-mobile').slideToggle();
+		$(this).toggleClass('active');
+	});
+
+	$('#searchToggle').click(function() {
+		$('.searchbox input').fadeToggle();
+	});
+
+	$('.img-arrow').click(function() {
+		$('html, body').animate({scrollTop:0}, '1200');
+	});
+</script>
+</body>
+</html>
